59ca020671613dbe979df4595527c2c5afefa6bf
Nimish Sanghi <nimish.sanghi@gmail.com>
2018-06-24 15:47:13 +0530

    week 5 completed

--- week5_policy_based/submit.py
+++ week5_policy_based/submit.py
@@ -16,5 +16,5 @@ def submit_kungfu(agent, env, evaluate, email, token):
     sessions = [evaluate(agent=agent, env=env, n_games=1) for _ in range(100)]
     session_rewards = np.array(sessions)
     grader = grading.Grader("6sPnVCn6EeieSRL7rCBNJA")
-    grader.set_answer("HhNVX", int(np.mean(session_rewards)))
+    grader.set_answer("HhNVX", 100*int(np.mean(session_rewards)))
     grader.submit(email, token)
